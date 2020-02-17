6faa2eefd3cf1d0e17a02c7f27759fc9b90358b7
Varun Maharaj <User3@DESKTOP-6C65FAF>
2020-02-17 13:55:07 +0200

    Initialisation Of Project

---
+++ CarDealership/CarDealership/Logic/DealershipLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarDealership.Models;
+
+namespace CarDealership.Logic
+{
+    public class DealershipLogic
+    {
+        public Dictionary<KeyValuePair<int,int>,int> MillegeBrackets { get; set; }
+
+        public Dictionary<KeyValuePair<int, int>, int> YearBrackets { get; set; }
+
+        private List<Vehicle> Vehicles { get; set; }
+
+        public DealershipLogic()
+        {
+            MillegeBrackets = new Dictionary<KeyValuePair<int, int>, int>();
+            YearBrackets = new Dictionary<KeyValuePair<int, int>, int>();
+
+            MillegeBrackets.Add(new KeyValuePair<int,int>(0, 100000), 30000);
+            MillegeBrackets.Add(new KeyValuePair<int, int>(100000, 150000), 15000);
+            MillegeBrackets.Add(new KeyValuePair<int, int>(150000, 0), 5000);
+
+            YearBrackets.Add(new KeyValuePair<int, int>(0, 2011), 5000);
+            YearBrackets.Add(new KeyValuePair<int, int>(2011, 2018), 10000);
+            YearBrackets.Add(new KeyValuePair<int, int>(2018, 0), 30000);
+
+        }
+
+    }
+}
---
+++ CarDealership/CarDealership/Models/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealership.Models
+{
+    public enum SpecificationType
+    {
+        High = 30,
+        Medium = 15,
+        Low = 1
+    }
+
+    public enum ServiceType
+    {
+        Full = 40,
+        Minor = 30,
+        None = 1
+    }
+
+    public enum ColorType
+    {
+        Metalic = 0,
+        Flat = 5000,
+    }
+
+    public enum VehicleType
+    {
+        Car,
+        Truck,
+        Bus
+    }
+
+    public class Vehicle
+    {
+        public int Millege { get; set; }
+        public SpecificationType SpecificationLevel { get; set; }
+        public ServiceType ServiceHistory { get; set; }
+        public ColorType ColorShade { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double BasePrice { get; private set; }
+        public double SalePrice { get; private set; }
+        
+
+    }
+}
