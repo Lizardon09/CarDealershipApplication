f6df0aad071bdbd85163893b8c35942a6c77daea
Varun Maharaj <varun@DESKTOP-AER3FMQ>
2020-02-17 23:38:33 +0200

    Added Dictionaries

--- CarDealership/CarDealership/Logic/DealershipLogic.cs
+++ CarDealership/CarDealership/Logic/DealershipLogic.cs
@@ -7,24 +7,38 @@ namespace CarDealership.Logic
 {
     public class DealershipLogic
     {
-        public Dictionary<KeyValuePair<int,int>,int> MillegeBrackets { get; set; }
+        public static Dictionary<KeyValuePair<int, int>, int> MillegeBrackets { get; set; }
+        public static Dictionary<KeyValuePair<int, int>, int> YearBrackets { get; set; }
 
-        public Dictionary<KeyValuePair<int, int>, int> YearBrackets { get; set; }
+        public static Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>> ExtraMillegeCosts { get; set; }
+        public static Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>> ExtraYearCosts { get; set; }
 
         private List<Vehicle> Vehicles { get; set; }
 
         public DealershipLogic()
         {
-            MillegeBrackets = new Dictionary<KeyValuePair<int, int>, int>();
-            YearBrackets = new Dictionary<KeyValuePair<int, int>, int>();
+            Vehicle.DefaultMillegeBrackets = new Dictionary<KeyValuePair<int, int>, int>();
+            Vehicle.DefaultYearBrackets = new Dictionary<KeyValuePair<int, int>, int>();
 
-            MillegeBrackets.Add(new KeyValuePair<int,int>(0, 100000), 30000);
-            MillegeBrackets.Add(new KeyValuePair<int, int>(100000, 150000), 15000);
-            MillegeBrackets.Add(new KeyValuePair<int, int>(150000, 0), 5000);
+            Vehicle.DefaultMillegeBrackets.Add(new KeyValuePair<int, int>(0, 100000), 30000);
+            Vehicle.DefaultMillegeBrackets.Add(new KeyValuePair<int, int>(100000, 150000), 15000);
+            Vehicle.DefaultMillegeBrackets.Add(new KeyValuePair<int, int>(150000, 0), 5000);
 
-            YearBrackets.Add(new KeyValuePair<int, int>(0, 2011), 5000);
-            YearBrackets.Add(new KeyValuePair<int, int>(2011, 2018), 10000);
-            YearBrackets.Add(new KeyValuePair<int, int>(2018, 0), 30000);
+            Vehicle.DefaultYearBrackets.Add(new KeyValuePair<int, int>(0, 2011), 5000);
+            Vehicle.DefaultYearBrackets.Add(new KeyValuePair<int, int>(2011, 2018), 10000);
+            Vehicle.DefaultYearBrackets.Add(new KeyValuePair<int, int>(2018, 0), 30000);
+
+            Vehicle.ExtraMillegeCosts = new Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>>();
+            Vehicle.ExtraYearCosts = new Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>>();
+
+            Dictionary<KeyValuePair<int, int>, Dictionary<VehicleType,int>> ExtraMillegeTruck = new Dictionary<KeyValuePair<int, int>, Dictionary<VehicleType, int>>();
+            Dictionary<KeyValuePair<int, int>, Dictionary<VehicleType, int>> ExtraYear = new Dictionary<KeyValuePair<int, int>, Dictionary<VehicleType, int>>();
+
+
+        }
+
+        public void CalculateSalePrice(Vehicle vehicle)
+        {
 
         }
 
--- CarDealership/CarDealership/Models/Vehicle.cs
+++ CarDealership/CarDealership/Models/Vehicle.cs
@@ -33,16 +33,30 @@ namespace CarDealership.Models
 
     public class Vehicle
     {
+        public static int ID { get; set; }
+        public static Dictionary<KeyValuePair<int, int>, int> DefaultMillegeBrackets { get; set; }
+        public static Dictionary<KeyValuePair<int, int>, int> DefaultYearBrackets { get; set; }
+
+        public static Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>> ExtraMillegeCosts { get; set; }
+        public static Dictionary<VehicleType, Dictionary<KeyValuePair<int, int>, int>> ExtraYearCosts { get; set; }
+        public static Dictionary<VehicleType, KeyValuePair<SpecificationType,int>> ExtraSpecificationCost { get; set; }
+        public static Dictionary<VehicleType, KeyValuePair<ServiceType, int>> ExtraServiceHistoryCost { get; set; }
+
         public int Millege { get; set; }
         public SpecificationType SpecificationLevel { get; set; }
         public ServiceType ServiceHistory { get; set; }
         public ColorType ColorShade { get; set; }
+        public VehicleType TypeOfVehicle { get; set; }
         public int Year { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public double BasePrice { get; private set; }
         public double SalePrice { get; private set; }
         
+        public Vehicle()
+        {
+            ID++;
+        }
 
     }
 }
